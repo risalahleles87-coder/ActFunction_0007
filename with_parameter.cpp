c5b09823aea3b56516d835ecfd14341632ffea7e
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-02 12:55:42 +0700

    with 1

--- with_parameter.cpp
+++ with_parameter.cpp
@@ -3,26 +3,3 @@ using namespace std;
 
 int panjang, lebar;
 
-void input()
-{
-    cout << "masukkan panjang :";
-    cin >> panjang;
-    cout << "masukkan lebar :";
-    cin >> lebar;
-}
-
-int luaspersegi()
-{
-    return panjang * lebar;
-}
-
-void output()
-{
-    cout << "hasilnya : " <<luaspersegi() << endl;
-}
-
-int main()
-{
-    input();
-    output();
-}
