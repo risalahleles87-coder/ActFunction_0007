55cbdd044e44cf63c823087459eda84411928e67
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-02 13:02:42 +0700

    ll 2

--- luas_lingkaran.cpp
+++ luas_lingkaran.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 float r;
 float phi = 3.14159;
+void input()
+{
+    cout << "isikan jari jari : ";
+    cin >> r;
+}
+
+float luas(float a)
+{
+    return phi * a * a;
+}
+
+
