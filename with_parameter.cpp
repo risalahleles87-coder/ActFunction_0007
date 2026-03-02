8fe3fb013a40b22a7bc19277aace78155e7388a8
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-02 12:56:54 +0700

    with 2

--- with_parameter.cpp
+++ with_parameter.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 int panjang, lebar;
 
+void input()
+{
+    cout << "masukkan panjang :";
+    cin >> panjang;
+    cout << "masukkan lebar :";
+    cin >> lebar;
+}
+
+int luaspersegi()
+{
+    return panjang * lebar;
+}
